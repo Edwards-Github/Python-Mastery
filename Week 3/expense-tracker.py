841da6128dc96ab97c5dd086529a09edbab7c556
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2026-03-03 16:26:32 -0800

    added filter category and monthly totals methods

--- Week 3/expense-tracker.py
+++ Week 3/expense-tracker.py
@@ -7,7 +7,7 @@ class Expense:
 		self.date = date
 
 	def __repr__(self):
-		return f'Expense({self.date}, {self.category}, {self.description}, ${self.amount})'
+		return f'{self.date}: [{self.category}] {self.description} ${self.amount}'
 
 class ExpenseTracker:
 
@@ -21,14 +21,37 @@ class ExpenseTracker:
 		for i, expense in enumerate(self.expenses, 1):
 			print(f'{i}.Date: {expense.date} Category: {expense.category} Description: {expense.description} Price: ${expense.amount}')
 
+	def filter_category(self, category):
+		return {category: [e for e in self.expenses if e.category == category]}
+
+	def monthly_totals(self):
+		months = ['January', 'February', 'March', 'April', ' May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+		monthly_dictionary = {}
+		for month in months:
+			monthly_dictionary[month] = []
+
+		for expense in self.expenses:
+			month = expense.date.split(' ')[0]
+			monthly_dictionary[month].append(expense.amount)
+
+		for month, expense in monthly_dictionary.items():
+			total = sum(monthly_dictionary[month])
+			monthly_dictionary[month] = total
+
+		return monthly_dictionary
+
 	def total(self):
 		return sum(expense.amount for expense in self.expenses)
 
 
 amazon = Expense(23, 'Amazon', 'ear phones', 'March 2, 2026')
 amazon_2 = Expense(160, 'Amazon', 'headphones', 'March 3, 2026')
+gamestop = Expense(60, 'Gamestop', 'Resident Evil 9', 'March 6, 2026')
 tracker = ExpenseTracker()
 tracker.add(amazon)
 tracker.add(amazon_2)
+tracker.add(gamestop)
 tracker.list()
 print(f'The total expense is: ${tracker.total()}')
+print(tracker.filter_category('Amazon'))
+print(tracker.monthly_totals())
