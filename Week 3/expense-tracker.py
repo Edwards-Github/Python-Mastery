90183efdf0dbf9f8d701424080967736807db58b
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2026-03-05 16:06:32 -0800

    1. Added defaultdict-powered categories to ExpenseTracker 2. Filter expenses by date range 3. Add export to CSV feature using file I/O

--- Week 3/expense-tracker.py
+++ Week 3/expense-tracker.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import date
 
 class Expense:
 
@@ -19,32 +20,48 @@ class ExpenseTracker:
 	def add(self, expense):
 		self.expenses.append(expense)
 
-	def list(self):
+	def display_expenses(self):
 		for i, expense in enumerate(self.expenses, 1):
 			print(f'{i}.Date: {expense.date} Category: {expense.category} Description: {expense.description} Price: ${expense.amount}')
 
-	def filter_category(self, category):
-		return {category: [e for e in self.expenses if e.category == category]}
+	def group_by_category(self, category):
+		grouped = defaultdict(list)
+		for e in self.expenses:
+			grouped[e.category].append(e)
+		return dict(grouped)
+
+	def filter_dates(self, start, end):
+		return sorted([d for d in self.expenses if start <= d.date <= end], key=lambda d: d.date)
 
 	def monthly_totals(self):
 		totals = defaultdict(float)
 		for expense in self.expenses:
-			month = expense.date.split(' ')[0]
+			month = (expense.date.year, expense.date.month)
 			totals[month] += expense.amount
 		return dict(totals)
 
 	def total(self):
 		return sum(expense.amount for expense in self.expenses)
 
+	def export_to_csv(self):
+		with open('ExpenseTracker.txt', 'w') as f:
+			f.write('date,category,description,amount\n')
+			for expense in self.expenses:
+				f.write(f'{expense.date},{expense.category},{expense.description},{expense.amount}\n')
 
-amazon = Expense(23, 'Amazon', 'ear phones', 'March 2, 2026')
-amazon_2 = Expense(160, 'Amazon', 'headphones', 'March 3, 2026')
-gamestop = Expense(60, 'Gamestop', 'Resident Evil 9', 'March 6, 2026')
+
+amazon = Expense(23, 'Amazon', 'ear phones', date(2026, 3, 2))
+amazon_2 = Expense(160, 'Amazon', 'headphones', date(2026, 3, 3))
+gamestop = Expense(60, 'Gamestop', 'Resident Evil 9', date(2026, 3, 6))
+gamestop2 = Expense(60, 'Gamestop', 'Resident Evil 9', date(2026, 4, 6))
 tracker = ExpenseTracker()
 tracker.add(amazon)
 tracker.add(amazon_2)
 tracker.add(gamestop)
-tracker.list()
+tracker.add(gamestop2)
+tracker.display_expenses()
 print(f'The total expense is: ${tracker.total()}')
-print(tracker.filter_category('Amazon'))
+print(tracker.group_by_category('Amazon'))
 print(tracker.monthly_totals())
+print(tracker.filter_dates(date(2026, 3, 1), date(2026, 4, 1)))
+tracker.export_to_csv()
