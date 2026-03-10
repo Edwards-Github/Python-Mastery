f171e6891f42a98b92ae70857ff79c573a1000ad
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2026-03-09 20:48:43 -0700

    refactored Contact Manager into a proper module:contacts.py, utils.py, main.py

--- Week 4/Contact Manager/main.py
+++ Week 4/Contact Manager/main.py
@@ -1,16 +1,17 @@
-import contacts.py, utils.py
+from contacts import add_contact, find, edit, list_contacts, create_contacts
+from utils import print_divider
 
 def main():
 	contacts = create_contacts()
 
-	add_contacts(contacts, 'Edward', ['123-4567', 'edward@gmail.com'])
-	add_contacts(contacts, 'Chris', ['456-7890', 'chris@gmail.com'])
+	add_contact(contacts, 'Edward', ['123-4567', 'edward@gmail.com'])
+	add_contact(contacts, 'Chris', ['456-7890', 'chris@gmail.com'])
 
 	print(find(contacts, 'Edward'))
 	print(find(contacts, 'Chris'))
 
 	edit(contacts, 'John', ['111-2222', 'john@gmail.com'])
-	add_contacts(contacts, 'John', ['111-2222', 'john@gmail.com'])
+	add_contact(contacts, 'John', ['111-2222', 'john@gmail.com'])
 	edit(contacts, 'John', ['111-2222', 'john@gmail.com'])
 
 	list_contacts(contacts)
